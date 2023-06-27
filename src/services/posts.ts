787d0150c0fb3ca65bcaec88502c17568861b085
rooney <bch3454@gmail.com>
2023-06-27 16:47:53 +0900

    Feat: 다음 포스트 카드

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -10,32 +10,40 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
+
+export async function getFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts() //
+    .then((posts) => posts.filter((post) => post.featured));
+}
+
+export async function getNonFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts() //
+    .then((posts) => posts.filter((post) => !post.featured));
+}
 
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "data", "posts.json");
   return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
-    .then((res) => res.sort((a, b) => (a.date > a.date ? -1 : 1)));
-}
-
-export async function getFeaturedPosts(): Promise<Post[]> {
-  return getAllPosts().then((posts) => posts.filter((post) => post.featured));
-}
-
-export async function getNonFeaturedPosts(): Promise<Post[]> {
-  return getAllPosts().then((posts) => posts.filter((post) => !post.featured));
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
-  const metadata = await getAllPosts().then((posts) =>
-    posts.find((post) => post.path === fileName)
-  );
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
 
-  if (!metadata)
-    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length ? posts[index + 1] : null;
   const content = await readFile(filePath, "utf-8");
 
-  return { ...metadata, content };
+  return { ...post, content, next, prev };
 }
